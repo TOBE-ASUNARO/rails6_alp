1e97977b728c644014630618e735562c5d39b614
TOBE-ASUNARO <ghnoadd@m5.asunaro-work.com>
2021-03-06 21:22:11 +0900

    good new start!

--- _roothome/atonosefish.sh
+++ _roothome/atonosefish.sh
@@ -6,8 +6,5 @@ fisher install 0rax/fish-bd && \
 fisher install jethrokuan/fzf && \
 fisher install ryotako/fish-global-abbreviation
 
-gabbr l exa
-cd
-echo gabbrdata >> ~/.config/fish/.gabbr.config
-gabbr -r
+gabbr l exa && cd && cat gabbrdata >> ~/.config/fish/.gabbr.config && gabbr -r
 
