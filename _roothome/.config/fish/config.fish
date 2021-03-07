8fa71f268576c18883bd709586e484da453561e4
TOBE-ASUNARO <ghnoadd@m5.asunaro-work.com>
2021-03-07 11:58:22 +0900

    boot test 3 for MAC

--- _roothome/.config/fish/config.fish
+++ _roothome/.config/fish/config.fish
@@ -1,18 +1,18 @@
 export LSCOLORS=Gxfxcxdxbxegedabagacad
 
 
-# fzf 
-set -U FZF_LEGACY_KEYBINDINGS 0
-set -U FZF_REVERSE_ISEARCH_OPTS "--reverse --height=100%"
+# # fzf 
+# set -U FZF_LEGACY_KEYBINDINGS 0
+# set -U FZF_REVERSE_ISEARCH_OPTS "--reverse --height=100%"
 
 
-#gabbr plugib
-bind ' ' '__gabbr_expand; commandline -i " "'
-bind ';' '__gabbr_expand; commandline -i ";"'
-bind '>' '__gabbr_expand; commandline -i ">"'
-bind '<' '__gabbr_expand; commandline -i "<"'
-bind '(' '__gabbr_expand; commandline -i "("'
-bind ')' '__gabbr_expand; commandline -i ")"'
-#gabbr 設定書き出し
-set -U gabbr_config ~/.config/fish/.gabbr.config
-#gabbr --reload
+# #gabbr plugib
+# bind ' ' '__gabbr_expand; commandline -i " "'
+# bind ';' '__gabbr_expand; commandline -i ";"'
+# bind '>' '__gabbr_expand; commandline -i ">"'
+# bind '<' '__gabbr_expand; commandline -i "<"'
+# bind '(' '__gabbr_expand; commandline -i "("'
+# bind ')' '__gabbr_expand; commandline -i ")"'
+# #gabbr 設定書き出し
+# set -U gabbr_config ~/.config/fish/.gabbr.config
+# #gabbr --reload
